f3e8471346edece81f375380257c32fa2b65c825
Gilles De Pessemier <gilles.depessemier@gmail.com>
2018-10-24 18:30:20 +0200

    Add files via upload

---
+++ Server.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnBroadcast = new System.Windows.Forms.Button();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.rtxtMessages = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 42);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(414, 20);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.Text = "Text";
+            // 
+            // btnBroadcast
+            // 
+            this.btnBroadcast.Location = new System.Drawing.Point(434, 40);
+            this.btnBroadcast.Name = "btnBroadcast";
+            this.btnBroadcast.Size = new System.Drawing.Size(75, 23);
+            this.btnBroadcast.TabIndex = 1;
+            this.btnBroadcast.Text = "Broadcast";
+            this.btnBroadcast.UseVisualStyleBackColor = true;
+            this.btnBroadcast.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(9, 9);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(41, 13);
+            this.lblServer.TabIndex = 2;
+            this.lblServer.Text = "Server:";
+            // 
+            // rtxtMessages
+            // 
+            this.rtxtMessages.Location = new System.Drawing.Point(12, 90);
+            this.rtxtMessages.Name = "rtxtMessages";
+            this.rtxtMessages.Size = new System.Drawing.Size(497, 159);
+            this.rtxtMessages.TabIndex = 3;
+            this.rtxtMessages.Text = "Messages\n";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 261);
+            this.Controls.Add(this.rtxtMessages);
+            this.Controls.Add(this.lblServer);
+            this.Controls.Add(this.btnBroadcast);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Server_FormClosing);
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnBroadcast;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.RichTextBox rtxtMessages;
+    }
+}
+
---
+++ Server.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetworkCommsDotNet.Tools;
+using NetworkCommsDotNet.Connections;
+using NetworkCommsDotNet;
+namespace Server
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+        string ip = "127.0.0.1";
+        int port = 6600;
+        private static List<Connection> Clients { get; set; } = new List<Connection>();
+        private void Server_Load(object sender, EventArgs e)
+        {
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Messages", OnMessageReceived);
+            NetworkComms.AppendGlobalConnectionCloseHandler(OnConnectionClose);
+            NetworkComms.AppendGlobalConnectionEstablishHandler(OnConnectionOpen);
+
+            //Start listening for incoming connections
+            Connection.StartListening(ConnectionType.TCP, new System.Net.IPEndPoint(System.Net.IPAddress.Parse(ip), port));
+
+            lblServer.Text = "Server: " + ip + ":" + port;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var stringToSend = txtMessage.Text;
+            txtMessage.Clear();
+
+            //If the user has typed exit then we leave
+            foreach (var client in Clients)
+            {
+                client.SendObject("Messages", stringToSend);
+            }
+            rtxtMessages.Text += "Server: " + stringToSend + Environment.NewLine;
+        }
+
+        private void OnMessageReceived(PacketHeader packetHeader, Connection connection, string message)
+        {
+            SetText("Incoming message from " + connection.ToString() + " saying '" + message + "'." + Environment.NewLine);
+        }
+
+        private void OnConnectionClose(Connection connection)
+        {
+            Clients.Remove(connection);
+            SetText("Client disconnected " + connection.ToString() + Environment.NewLine);
+        }
+
+        private void OnConnectionOpen(Connection connection)
+        {
+            Clients.Add(connection);
+            SetText("Client connected " + connection.ToString() + Environment.NewLine);
+        }
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.rtxtMessages.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.rtxtMessages.Text += text;
+            }
+        }
+
+        private void Server_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // close connection
+            NetworkComms.Shutdown();
+        }
+    }
+}
