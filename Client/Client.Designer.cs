4f40311eefa5e5e1593e60c58c04a218bd27d193
Gilles De Pessemier <gilles.depessemier@gmail.com>
2018-10-24 18:35:46 +0200

    Add files via upload

---
+++ Client/Client.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.rtxtMessages = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 81);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(313, 20);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.Text = "Test";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(331, 79);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(159, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 20);
+            this.txtIP.TabIndex = 3;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(118, 12);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(35, 20);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "6600";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(9, 35);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(43, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status: ";
+            // 
+            // rtxtMessages
+            // 
+            this.rtxtMessages.Location = new System.Drawing.Point(12, 107);
+            this.rtxtMessages.Name = "rtxtMessages";
+            this.rtxtMessages.Size = new System.Drawing.Size(394, 142);
+            this.rtxtMessages.TabIndex = 7;
+            this.rtxtMessages.Text = "Messages\n";
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 261);
+            this.Controls.Add(this.rtxtMessages);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_FormClosing);
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.RichTextBox rtxtMessages;
+    }
+}
+
---
+++ Client/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using NetworkCommsDotNet.Tools;
+
+namespace Client
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+        string ip = "127.0.0.1";
+        int port = 6600;
+        private void Client_Load(object sender, EventArgs e)
+        {
+            // set the on message event
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Messages", OnMessageReceived);     
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ip = txtIP.Text;
+            port = Convert.ToInt16(txtPort.Text);
+
+            // first message to the server will trigger a connection automatically plus it will send the message
+            NetworkComms.SendObject("Messages", ip, port, "Connecting");
+            lblStatus.Text = "Status: " + ip + ":" + port;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            var stringToSend = txtMessage.Text;
+            txtMessage.Clear();
+
+            NetworkComms.SendObject("Messages", ip, port, stringToSend);
+            rtxtMessages.Text += "Me: " + stringToSend + Environment.NewLine;
+        }
+
+        private void OnMessageReceived(PacketHeader packetHeader, Connection connection, string message)
+        {
+            rtxtMessages.Text += "Incoming message from " + connection.ToString() + " saying '" + message + "'." + Environment.NewLine;
+        }
+
+        private void Client_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // close connection
+            NetworkComms.Shutdown();
+        }
+    }
+}
